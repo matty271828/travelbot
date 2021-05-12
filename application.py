9551fe055adf1f957e4a8cc224b1307e4e4d18e2
Matthew Maclean <macleanmatty@gmail.com>
2021-05-12 23:38:53 +0100

    Added findingCheapestFlights class

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 import requests
 import json
 import os
+import time, datetime, dateutil
+import pandas as pd
+from helpers import findingCheapestFlights
+
 
 api_key = os.environ.get("SKYSCANNER_API_KEY")
 
@@ -14,31 +18,44 @@ source_array = {"LPL-sky"}
 # Our destination airports: Madrid, Barcelona, Seville, Valencia
 destination_array = {"MAD-sky", "BCN-sky", "SVQ-sky", "VLC-sky"}
 
-
-
-# And to make our life easier
-rootURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/"
-currency = "GBP"
-originCountry = "UK"
-locale = "en-US"
-
+source_begin_date = "2021-09-18"
+source_end_date =  "2021-09-24"  
+daterange = pd.date_range(source_begin_date, source_end_date)
 airports = { }
-for destination in destination_array:
-    for source in source_array:
-        myurl = rootURL + originCountry + "/" + currency + "/" + locale + "/" + source + "/"  + destination + "/" + "2021-09-01"
-        response = requests.request("GET", myurl, headers=headers)
-        temp = json.loads(response.text)
-        
-        # This checks if we have a quote or there were no flights
-        if("Quotes" in temp):
-            for Places in temp["Places"]:
-                # Add the airport in the dictionary.
-                airports[Places["PlaceId"]] = Places["Name"] 
-            for Quotes in temp["Quotes"]:
-                print("************")
-                # print("%s --> to  -->%s" %(origin,destination))
-                ori = Quotes["OutboundLeg"]["OriginId"]
-                dest = Quotes["OutboundLeg"]["DestinationId"]
-                # Look for Airports in the dictionary
-                print("Journy:  %s  --> %s"%(airports[ori],airports[dest]))
-                print("Price: %s GBP" %Quotes["MinPrice"])
+maxbudget = 40
+
+cheapest_flight_finder = findingCheapestFlights()
+cheapest_flight_finder.setHeaders(headers)
+
+total_compute_time = 0.0
+total_request_time = 0.0
+
+function_start = time.time()
+for single_date in daterange:
+    for destination in destination_array:
+        for source in source_array:
+            request_start = time.time()
+            resultJSON = cheapest_flight_finder.browseQuotes(source, destination,single_date)
+            request_end = time.time()
+            if("Quotes" in resultJSON):
+                for Places in resultJSON["Places"]:
+                    # Add the airport in the dictionary.
+                    airports[Places["PlaceId"]] = Places["Name"] 
+                for Quotes in resultJSON["Quotes"]:
+                    if(Quotes["MinPrice"]<maxbudget):                        
+                        print("************")
+                        print(single_date.strftime("%d-%b %a"))
+                        # print("%s --> to  -->%s" %(origin,destination))
+                        source = Quotes["OutboundLeg"]["OriginId"]
+                        dest = Quotes["OutboundLeg"]["DestinationId"]
+                        # Look for Airports in the dictionary
+                        print("Journy:  %s  --> %s"%(airports[source],airports[dest]))
+                        print("Price: %s EUR" %Quotes["MinPrice"])
+            calculation_end = time.time()
+            total_compute_time += calculation_end - request_end 
+            total_request_time += request_end - request_start
+
+print("\nBenchmark Stats :")
+print("Time spent in computing: %f seconds"%total_compute_time )
+print("Time spent in requesting: %f seconds"%total_request_time )
+print("Time spent in program: %f seconds"%(time.time()-function_start))
--- helpers.py
+++ helpers.py
@@ -1,1 +1,19 @@
+import requests, datetime, json
 
+class findingCheapestFlights:
+    
+    def __init__(self, originCountry = "UK", currency = "GBP", locale = "en", rootURL="https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"):
+        self.currency = currency
+        self.locale =  locale
+        self.rootURL = rootURL
+        self.originCountry = originCountry
+
+    def setHeaders(self, headers):
+        self.headers =  headers
+
+    def browseQuotes(self, source, destination, date):
+        quoteRequestPath = "/apiservices/browsequotes/v1.0/"
+        browseQuotesURL = self.rootURL + quoteRequestPath + self.originCountry + "/" + self.currency + "/" + self.locale + "/" + source + "/" + destination + "/" + date.strftime("%Y-%m-%d")
+        response = requests.request("GET", url = browseQuotesURL, headers = self.headers)
+        resultJSON = json.loads(response.text)
+        return resultJSON
