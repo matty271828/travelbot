7d696ffc7de892723431b5ec7654d317ec99c6a6
Matthew Maclean <macleanmatty@gmail.com>
2021-05-12 23:04:40 +0100

    Further progress on tutorial

--- application.py
+++ application.py
@@ -9,12 +9,36 @@ headers = {
     'x-rapidapi-key':api_key
 }
 
-origin = "BERL-sky"
-destination = "LOND-sky"
+# Airports where we can fly from: Berlin
+source_array = {"LPL-sky"} 
+# Our destination airports: Madrid, Barcelona, Seville, Valencia
+destination_array = {"MAD-sky", "BCN-sky", "SVQ-sky", "VLC-sky"}
+
+
+
+# And to make our life easier
+rootURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/"
 currency = "GBP"
-originCountry = "DE"
+originCountry = "UK"
 locale = "en-US"
 
-myurl = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/" + originCountry + "/" + currency + "/" + locale + "/"  + destination + "/" + origin + "/"+ "2021-07-22"
-response = requests.request("GET", myurl, headers=headers)
-print(response.text)
+airports = { }
+for destination in destination_array:
+    for source in source_array:
+        myurl = rootURL + originCountry + "/" + currency + "/" + locale + "/" + source + "/"  + destination + "/" + "2021-09-01"
+        response = requests.request("GET", myurl, headers=headers)
+        temp = json.loads(response.text)
+        
+        # This checks if we have a quote or there were no flights
+        if("Quotes" in temp):
+            for Places in temp["Places"]:
+                # Add the airport in the dictionary.
+                airports[Places["PlaceId"]] = Places["Name"] 
+            for Quotes in temp["Quotes"]:
+                print("************")
+                # print("%s --> to  -->%s" %(origin,destination))
+                ori = Quotes["OutboundLeg"]["OriginId"]
+                dest = Quotes["OutboundLeg"]["DestinationId"]
+                # Look for Airports in the dictionary
+                print("Journy:  %s  --> %s"%(airports[ori],airports[dest]))
+                print("Price: %s GBP" %Quotes["MinPrice"])
