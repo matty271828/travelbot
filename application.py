8cf366f322d3adbee650f9734d2e4f8602570ecf
Matthew Maclean <macleanmatty@gmail.com>
2021-05-14 13:11:05 +0100

    Added threading capability

--- application.py
+++ application.py
@@ -3,7 +3,9 @@ import json
 import os
 import time, datetime, dateutil
 import pandas as pd
-from helpers import findingCheapestFlights
+import concurrent.futures, threading
+
+from helpers import finder
 
 
 api_key = os.environ.get("SKYSCANNER_API_KEY")
@@ -14,46 +16,33 @@ headers = {
 }
 
 # Airports where we can fly from: Berlin
-source_array = {"LPL-sky"} 
+source_array = {"BERL-sky"} 
 # Our destination airports: Madrid, Barcelona, Seville, Valencia
 destination_array = {"MAD-sky", "BCN-sky", "SVQ-sky", "VLC-sky"}
 
 source_begin_date = "2021-09-18"
 source_end_date =  "2021-09-24"  
-daterange = pd.date_range(source_begin_date, source_end_date)
+daterange_source = pd.date_range(source_begin_date, source_end_date)
 airports = { }
 maxbudget = 40
-
-cheapest_flight_finder = findingCheapestFlights()
-cheapest_flight_finder.setHeaders(headers)
 
 total_compute_time = 0.0
 total_request_time = 0.0
 
+cheapest_flight_finder2 = finder()
+cheapest_flight_finder2.setHeaders(headers)
+
 function_start = time.time()
-for single_date in daterange:
-    for destination in destination_array:
-        for source in source_array:
-            request_start = time.time()
-            resultJSON = cheapest_flight_finder.browseQuotes(source, destination,single_date)
-            request_end = time.time()
-            if("Quotes" in resultJSON):
-                for Places in resultJSON["Places"]:
-                    # Add the airport in the dictionary.
-                    airports[Places["PlaceId"]] = Places["Name"] 
-                for Quotes in resultJSON["Quotes"]:
-                    if(Quotes["MinPrice"]<maxbudget):                        
-                        print("************")
-                        print(single_date.strftime("%d-%b %a"))
-                        # print("%s --> to  -->%s" %(origin,destination))
-                        source = Quotes["OutboundLeg"]["OriginId"]
-                        dest = Quotes["OutboundLeg"]["DestinationId"]
-                        # Look for Airports in the dictionary
-                        print("Journy:  %s  --> %s"%(airports[source],airports[dest]))
-                        print("Price: %s EUR" %Quotes["MinPrice"])
-            calculation_end = time.time()
-            total_compute_time += calculation_end - request_end 
-            total_request_time += request_end - request_start
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
+    for single_date in daterange_source:
+        for destination in destination_array:
+            for source in source_array:
+                request_start = time.time()
+                executor.submit(cheapest_flight_finder2.browseQuotes,source, destination,single_date)
+
+print("\nBenchmark Stats :")
+print("Time spent in program: %f seconds"%(time.time()-function_start))
 
 print("\nBenchmark Stats :")
 print("Time spent in computing: %f seconds"%total_compute_time )
--- helpers.py
+++ helpers.py
@@ -1,19 +1,38 @@
 import requests, datetime, json
 
-class findingCheapestFlights:
+class finder:
     
-    def __init__(self, originCountry = "UK", currency = "GBP", locale = "en", rootURL="https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"):
+    def __init__(self, originCountry = "DE", currency = "EUR", locale = "en-US", rootURL="https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"):
         self.currency = currency
         self.locale =  locale
         self.rootURL = rootURL
         self.originCountry = originCountry
-
+        self.airports = {}
+        
     def setHeaders(self, headers):
         self.headers =  headers
+        self.createSession()
 
+    # Create a session
+    def createSession(self):
+        self.session = requests.Session() 
+        self.session.headers.update(self.headers)
+        return self.session
+        
     def browseQuotes(self, source, destination, date):
         quoteRequestPath = "/apiservices/browsequotes/v1.0/"
         browseQuotesURL = self.rootURL + quoteRequestPath + self.originCountry + "/" + self.currency + "/" + self.locale + "/" + source + "/" + destination + "/" + date.strftime("%Y-%m-%d")
-        response = requests.request("GET", url = browseQuotesURL, headers = self.headers)
+        # Use the same session to request again and again
+        response = self.session.get(browseQuotesURL)
         resultJSON = json.loads(response.text)
-        return resultJSON
+        self.printResult(resultJSON,date)
+        
+    # A bit more elegant print
+    def printResult(self, resultJSON,date):
+        if("Quotes" in resultJSON):
+            for Places in resultJSON["Places"]:
+                self.airports[Places["PlaceId"]] = Places["Name"] 
+            for Quotes in resultJSON["Quotes"]:
+                source = Quotes["OutboundLeg"]["OriginId"]
+                dest = Quotes["OutboundLeg"]["DestinationId"]
+                print(date.strftime("%d-%b %a") + " | " + "%s  --> %s"%(self.airports[source],self.airports[dest]) + " | " + "%s EUR" %Quotes["MinPrice"])
