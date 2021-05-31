4810e5c7f3d5163c8ea40e515cad1103feda6b35
Matthew Maclean <macleanmatty@gmail.com>
2021-05-31 17:05:11 +0100

    Merged changes

--- application.py
+++ application.py
@@ -4,24 +4,17 @@ import os
 import time, datetime, dateutil
 import pandas as pd
 
-<<<<<<< HEAD
 from functions import search_oneway, search_30dayreturn
-=======
-from functions import search_oneway, search_30dayreturn, search_oneyearreturn
->>>>>>> 2440315c307f7b0cf560fd5b836f0c783ecce7f2
 
 # Airports where we can fly from
 source_array = {"UK-sky"} 
 # Our destination airports
 destination_array = {"US-sky"}
 
-<<<<<<< HEAD
 # Dates
 source_begin_date = pd.to_datetime("2021-09-05")
 source_end_date =  pd.to_datetime("2021-09-07")
 
-=======
->>>>>>> 2440315c307f7b0cf560fd5b836f0c783ecce7f2
 # Define max budget, enter as None if not wanted
 max_budget = None
 
@@ -32,17 +25,9 @@ total_request_time = 0.0
 # Start timer
 function_start = time.time()
 
-<<<<<<< HEAD
 search_oneway(source_array, destination_array, source_begin_date, source_end_date)
 
 # search_30dayreturn(source_array, destination_array, source_begin_date, source_end_date, max_budget)
-=======
-# Dates
-source_begin_date = pd.to_datetime("2021-09-05")
-source_end_date =  pd.to_datetime("2021-10-05")
-
-search_30dayreturn(source_array, destination_array, source_begin_date, source_end_date, max_budget)
->>>>>>> 2440315c307f7b0cf560fd5b836f0c783ecce7f2
 
 # Stats on runtime
 print("\nBenchmark Stats :")
